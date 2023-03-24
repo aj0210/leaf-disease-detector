1d734a2640afbb049b6885534a480d3124b65705
aj0210 <70054954+aj0210@users.noreply.github.com>
2023-03-24 17:20:14 +0530

    New model and Ui changes

---
+++ about.md
@@ -0,0 +1,14 @@
+## About Project
+
+A tool for helping farmers, researchers, and botanists identify specific grape leaf diseases. The application delivers a grape leaf image to the model for analysis using the CNN classifier model. Once a condition has been identified, the user is presented with both the disease and a treatment option.
+---
+
+## Model
+
+The model is trained to detect 3 types of grape Leaf disease
+
+- Black rot
+- Esca(Black_Measles)
+- Leaf Blight(Isariopsis Leaf Spot)
+---
+
--- app.py
+++ app.py
@@ -1,42 +1,77 @@
 import streamlit as st
 import tensorflow as tf
-
-
-# st.set_option('deprection.showFileUploaderEncoding',False)
-st.cache(allow_output_mutation=True)
-
-
-def load_model():
-    new_model=tf.keras.models.load_model("leaf_disease_coloured.h5")
-    return new_model
-model=load_model()
-st.title("Grape Leaf Disease Detection")
-
-file=st.file_uploader("Please upload grape leaf image",type=["jpg",'png'])
 import cv2
-from PIL import Image,ImageOps
+from PIL import Image, ImageOps
 import numpy as np
+from pathlib import Path
 
-def import_and_predict(image_data,model):
-    size=(256,256)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    predict_class=np.argmax(model.predict(img_reshape),axis=1)
-    
-    return predict_class
+banner_path="banner.jpg"
+model=tf.keras.models.load_model("leaf_disease_coloured_24_3.h5")
 
-if file is None:
-    st.text("Please upload an image file")
-else:
-    image=Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,model)
-    categories=["Black_rot","Esca_(Black_Measles)","Healthy","Leaf_blight_(Isariopsis_Leaf_Spot)"]
+menu = ["Disease Detection","About"]
+choice = st.sidebar.selectbox("Select Activty", menu)
+def read_markdown_file(markdown_file):
+    return Path(markdown_file).read_text()
 
-    if categories[prediction[0]]=="Healthy":
-        s1="No Disease Detected"
-        st.success(s1)
+
+if choice=="About":
+    intro_markdown = read_markdown_file("about.md")
+    st.markdown(intro_markdown, unsafe_allow_html=True)
+elif choice=="Disease Detection":
+    original_title = '<p style="font-family:trebuchet ms; color:Black;text-align:center;font-size: 40px;">Grape Leaf Disease Diagnosis</p>'
+    st.markdown(original_title, unsafe_allow_html=True)
+
+    file = st.file_uploader("Upload image...", type=["jpg", 'png'])
+
+    def add_bg_from_url():
+        st.markdown(
+            f"""
+             <style>
+             .stApp {{
+                 background-image: url("https://cdn.wallpapersafari.com/5/78/njrDYe.jpg");
+                 background-attachment: fixed;
+                 background-size: cover
+             }}
+             </style>
+             """,
+            unsafe_allow_html=True
+        )
+
+    add_bg_from_url()
+    def import_and_predict(image_data, model):
+        size = (256, 256)
+        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+        image = np.asarray(image)
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        img_resize = cv2.resize(img, dsize=(256, 256), interpolation=cv2.INTER_CUBIC)
+        img_reshape = img_resize[np.newaxis, ...]
+        print(img_reshape)
+        predict_class = np.argmax(model.predict(img_reshape), axis=1)
+
+        return predict_class
+
+
+    if file is None:
+        img_none = '<p style="font-family:trebuchet ms; color:Black;text-align:center;font-size: 20px;">Please upload an grape leaf image</p>'
+        st.markdown(img_none, unsafe_allow_html=True)
     else:
-        string="This grape is having : "+categories[prediction[0]]
-        st.success(string)
+        image = Image.open(file)
+        st.sidebar.image(image, use_column_width=True)
+        prediction = import_and_predict(image, model)
+        categories = ["Black_rot", "Esca_(Black_Measles)", "Healthy", "Leaf_blight_(Isariopsis_Leaf_Spot)"]
+
+        if categories[prediction[0]] == "Healthy":
+            st1 = "No Disease detected"
+            st.sidebar.success(st1)
+        elif categories[prediction[0]] == "Black_rot":
+            stringbt = "This grape is having : Blackrot"
+            st.sidebar.success(stringbt)
+            st.title("Treatment")
+        elif categories[prediction[0]] == "Esca_(Black_Measles)":
+            stringbm = "This grape is having : Esca (Black_Measles)"
+            st.sidebar.success(stringbm)
+            st.title("Treatment")
+        elif categories[prediction[0]] == "Leaf_blight_(Isariopsis_Leaf_Spot)":
+            stringlb = "This grape is having : Leaf blight (Isariopsis Leaf Spot)"
+            st.sidebar.success(stringlb)
+            st.title("Treatment")
